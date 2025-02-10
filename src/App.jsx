0b0ae336b89cd3eb53a4ae7b2afb2e26c9e13d1b
Daxesh-Prajapati <daxeshhh.p@gmail.com>
2025-02-10 08:54:54 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 //import "./App.css";
-import {Header} from "./components/Header"
-import Footer from "./components/Footer";
-import { Content } from "./components/Content";
-import { ArrayDemo1 } from "./components/ArrayDemo1";
-import { ArrayDemo2 } from "./components/ArrayDemo2";
-import { ArrayDemo3 } from "./components/ArrayDemo3";
-import { ArrayDemo4 } from "./components/ArrayDemo4";
+import { Header } from './components/Header';
+import Footer from './components/Footer';
+import { Content } from './components/Content';
+import { ArrayDemo1 } from './components/ArrayDemo1';
+import { ArrayDemo2 } from './components/ArrayDemo2';
+import { ArrayDemo3 } from './components/ArrayDemo3';
+import { ArrayDemo4 } from './components/ArrayDemo4';
+import { UseStateDemo } from './components/UseStateDemo';
+import { UseStateDemo2 } from './components/UseStateDemo2';
 
 function App() {
   //js
-
-
 
   return (
     <div>
@@ -22,7 +22,9 @@ function App() {
       {/* <ArrayDemo1></ArrayDemo1> */}
       {/* <ArrayDemo2></ArrayDemo2> */}
       {/* <ArrayDemo3></ArrayDemo3> */}
-      <ArrayDemo4/>
+      {/* <ArrayDemo4/> */}
+      {/* <UseStateDemo></UseStateDemo> */}
+      <UseStateDemo2></UseStateDemo2>
       {/* <Footer></Footer> */}
     </div>
   );
